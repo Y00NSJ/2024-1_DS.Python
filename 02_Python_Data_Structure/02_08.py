30e3235ab0e2e0a75b16ed3060ed5bf27587a08e
Yoon <ysj0509@sookmyung.ac.kr>
2024-03-28 04:45:35 +0900

    fix
    `term_add(poly)` 함수는 리스트가 최고차항부터(튜플의 [1])
    내림차순으로 정렬되어 있단 가정 하에 덧셈을 수행합니다.
    `.sort().reverse()` 는 지수(튜플의 [0]) 내림차순으로 정렬하므로,
    최고차항 내림차순으로 정리하는 과정이 추가적으로 필요합니다.

--- 02_Python_Data_Structure/02_08.py
+++ 02_Python_Data_Structure/02_08.py
@@ -6,31 +6,44 @@ q = [(7,3), (2,2)]
 
 #def term_mul():
 
+
 def term_add(poly):
+    tlist = [] #연산 결과 리스트
     for i in range(1, len(poly)):
-        if poly[i][2] == poly[i-1][2]:
-            poly[i][1] += poly[i-1][1]
-            poly.remove(poly[i-1])
-    return poly
+        if poly[i][1] == poly[i-1][1]:  #지수가 같으면
+            tlist.append((poly[i][0] + poly[i-1][0], poly[i][1])) #계수덧셈
+            poly[i] = (0, 0) #리스트 중복추가 방지
+            i += 1 #점프
+        else:
+            if poly[i-1] == (0, 0): #리스트 중복추가 방지
+                continue
+            else:
+                 tlist.append((poly[i-1][0], poly[i-1][1])) #동차수항 없으면 바로 추가
+    return tlist
 
 
 def poly_add(p,q):
-    result = []
-    result = (p+q).sort()       #[(9,4),(7,3),(5,3),(4,2),(2,2)]
-    term_add(result)
-    return result
+    result = p+q
+    result.sort()       #[(9,4),(7,3),(5,3),(4,2),(2,2)]
+    result.reverse()    #내림차순 정렬하기
+    
+    return term_add(result)
+
 
 def poly_mul(p,q):
     result = []
-    for term1 in p:
-        for term2 in q:
-            term1[0]*=term1[0]
-            term1[1]+=term2[1]
-            result.add[term1]
-    result.sort()
-    term_add(result)
-    return result
 
+    for i in range(0,len(p)):
+        for j in range(0,len(q)):
+            result.append((p[i][0]*q[j][0], p[i][1]+q[j][1]))
+
+    result.sort()
+    print(result)
+    result.reverse()
+    print(result)
+    #term_add(result)
+    #return result
 
 print("P + Q = ", poly_add(p,q))
-print("P + Q = ", poly_mul(p,q))
+poly_mul(p,q)
+#print("P * Q = ", poly_mul(p,q))
