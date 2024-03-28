7c2ae3cc54073a0182d94d778df2099f6c59d4ee
Yoon <ysj0509@sookmyung.ac.kr>
2024-03-29 03:04:30 +0900

    [FIX] max 변수의 initializing 문제
    max 변수는 단 1회만 initialize 되어야 하므로
    재귀함수로 구현하기 부적절하다.
    따라서 리스트 값을 업데이트하는 방향으로 재구성하였다.
    허나, 결과값이 출력되지 않는(NONE) 문제가 발생하고 있다.

--- 03_Recursion/03_02.py
+++ 03_Recursion/03_02.py
@@ -1,11 +1,14 @@
 def rmax(lst):
-    if len(lst) == 0:       #lst에 원소가 없으면
-        return max          #max 출력
+    if len(lst) == 1:       #lst에 원소가 하나만 있다면
+        return lst[0]          #max 출력
     else:
-        if lst[0] >= max:    #lst[0] >= max 이면
-            max = lst[0]          #lst[0]을 max로
-        rmax(lst[1:])               #lst[0] 빼고 재귀!
+        if lst[1] >= lst[0]:    #lst[0] >= max 이면
+            rmax(lst[1:])
+        elif lst[1] < lst[0]:
+            lst[1] = lst[0]
+            rmax(lst[1:])
+
+
 
 findList = [3, 1, 6, 5]
-
-print(rmax(findList))
+rmax(findList)
