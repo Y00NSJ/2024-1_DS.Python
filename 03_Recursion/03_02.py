fd4da8de70fbbd51ad029c3eb6fb0439e28b4f45
Yoon <ysj0509@sookmyung.ac.kr>
2024-03-29 03:19:30 +0900

    min 계산 함수 추가
    작성한 함수들은 리스트 값을 변경하므로, 두 함수 합쳐 한 번만 실행 가능하다.

--- 03_Recursion/03_02.py
+++ 03_Recursion/03_02.py
@@ -1,14 +1,22 @@
 def rmax(lst):
-    if len(lst) == 1:       #lst에 원소가 하나만 있다면
-        return lst[0]          #max 출력
+    if len(lst) == 1:               #lst에 원소가 하나만 있다면
+        return lst[0]               #max 출력 <escape>
     else:
-        if lst[1] >= lst[0]:    #lst[0] >= max 이면
-            return rmax(lst[1:])
-        elif lst[1] < lst[0]:
+        if lst[1] >= lst[0]:        #lst[1]이 최댓값이면
+            return rmax(lst[1:])    #lst[1:]로 재귀호출
+        elif lst[1] < lst[0]:       #lst[0]이 최댓값이면
+            lst[1] = lst[0]         #lst[1]을 lst[0]값으로 바꾸고
+            return rmax(lst[1:])    #lst[1:]로 재귀호출
+
+def rmin(lst):
+    if len(lst) == 1:       
+        return lst[0]          
+    else:
+        if lst[1] <= lst[0]:    
+            return rmin(lst[1:])
+        elif lst[1] > lst[0]:
             lst[1] = lst[0]
-            return rmax(lst[1:])
-
-
+            return rmin(lst[1:])
 
 findList = [3, 1, 6, 5]
-print(rmax(findList))
+print("최댓값 :", rmax(findList), "최솟값 :", rmin(findList))
