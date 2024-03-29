108746f03ff19d47696e44ed92b9bfe18ea50515
Yoon <ysj0509@sookmyung.ac.kr>
2024-03-30 05:37:03 +0900

    깊은 복사 대신 swap 사용

--- 03_Recursion/03_02.py
+++ 03_Recursion/03_02.py
@@ -1,26 +1,22 @@
-import copy
-
 def rmax(lst):
-    lst = copy.deepcopy(lst)
-    if len(lst) == 1:               #lst에 원소가 하나만 있다면
-        return lst[0]               #max 출력 <escape>
+    if len(lst) == 1:                           # lst에 원소가 하나만 있다면
+        return lst[0]                           # max 출력 <escape>
     else:
-        if lst[1] >= lst[0]:        #lst[1]이 최댓값이면
-            return rmax(lst[1:])    #lst[1:]로 재귀호출
-        elif lst[1] < lst[0]:       #lst[0]이 최댓값이면
-            lst[1] = lst[0]         #lst[1]을 lst[0]값으로 바꾸고
-            return rmax(lst[1:])    #lst[1:]로 재귀호출
+        if lst[1] >= lst[0]:                    # lst[1]이 최댓값이면
+            return rmax(lst[1:])                # lst[1:]로 재귀호출
+        elif lst[1] < lst[0]:                   # lst[0]이 최댓값이면
+            lst [0], lst[1] = lst[1], lst[0]    # lst[1]을 lst[0]값으로 바꾸고
+            return rmax(lst[1:])                # lst[1:]로 재귀호출
 
 def rmin(lst):
-    lst = copy.deepcopy(lst)
     if len(lst) == 1:       
         return lst[0]          
     else:
         if lst[1] <= lst[0]:    
             return rmin(lst[1:])
         elif lst[1] > lst[0]:
-            lst[1] = lst[0]
+            lst[0], lst[1] = lst[1], lst[0]
             return rmin(lst[1:])
 
-findList = [3, 1, 6, 5]
-print("최댓값 :", rmax(findList), "최솟값 :", rmin(findList))
+findList = [21, 7, 40, 29, 11, 5, 90, 78, 64, 15, 88]
+print("최댓값 :", rmax(findList), "\n최솟값 :", rmin(findList))
