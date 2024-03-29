8a7d1c6c6be8accf76f2ca9f2ea692ec945e3dad
Yoon <ysj0509@sookmyung.ac.kr>
2024-03-30 00:47:14 +0900

    프레임별 점수 및 결과 + 총점 출력

--- 02_Python_Data_Type/Bowling.py
+++ 02_Python_Data_Type/Bowling.py
@@ -1,23 +1,34 @@
 score1 = [(8, 0), (4, 3), (8, 2), (4, 6), (2, 6), 
           (10, 0), (9, 0), (10, 0), (8, 2), (10, 0), 
           (10, 10)] # bonus throw
+score2 = [(10, 0), (10, 0), (10, 0), (10, 0), (10, 0), 
+          (10, 0), (10, 0), (10, 0), (10, 0), (10, 0), 
+          (10, 10)] # bonus throw
 
 
 def frame_scores(score):
     game = []
     itotal = 0
+    total = 0
+    #result = ""
     for i in range(10):
         itotal = score[i][0] + score[i][1]
 
         if itotal == 10:                                #strike / spare
             if score[i][0] == 10:                       #strike(10프레임 포함)
                 itotal += score[i+1][0] + score[i+1][1]
-                if i < 9 and score[i+1][0] == 10:      #1~9프레임에서 더블
+                if i < 9 and score[i+1][0] == 10:       #1~9프레임에서 더블
                     itotal += score[i+2][0]
+                game.append((itotal, "STRIKE"))
             
-            else:
+            else:                                       #spare
                 itotal += score[i+1][0]
-        game.append(itotal)
-    return game
+                game.append((itotal, "SPARE"))
+        else:                                           #none
+            game.append((itotal, "NONE"))
+        
+        total += itotal
+    print(game)
+    print("Total = ", total)
 
-frame_scores(score1)
+frame_scores(score2)
