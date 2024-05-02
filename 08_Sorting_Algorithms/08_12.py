25d4ac9da0672ebee3addc283d3e397262c6f23a
Yoon <ysj0509@sookmyung.ac.kr>
2024-05-02 19:49:12 +0900

    12. 최대값 우선 선택 정렬 함수 작성하기
    리스트의 왼쪽 수를 기준 수로 정하고 최댓값과 교환

---
+++ 08_Sorting_Algorithms/08_12.py
@@ -0,0 +1,27 @@
+# 최댓값 우선 선택 정렬
+class SelectionSort:
+    def __init__(self, num):
+        self.num = num
+        self.size = len(num)
+        print("Selection Sort", self.num)   # 정렬 전 리스트
+
+    def __str__(self):
+        for i in range(self.size):
+            print("$2d " % self.num[i])
+
+    def swap(self, a, b):
+        self.num[a], self.num[b] = self.num[b], self.num[a]
+
+    def sort(self):
+        n = self.size
+        for i in range(n - 1):
+            max = i     # 최솟값 우선 선택
+            for j in range(i + 1, n):
+                if self.num[j] > self.num[max]:
+                    max = j
+            self.swap(i, max)
+            print(self.num)
+
+num = [31, 10, 9, 23, 49, 15, 11, 7]
+s = SelectionSort(num)
+s.sort()
